2de540d94ab2162a82ce269ec2d17e94e2761e99
FedorTrain <fedya_dryagin@mail.ru>
2019-10-24 03:44:37 +0500

    Hmm...

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,15 @@
 $('document').ready(function() {
   $('#new').click(function() {
-    alert('working');
-
-
+    $('#content').append('<p>We dont have news</p>');
   });
-
+  $('#stock').click(function() {
+    $('#content').append('<p>We dont have stock</p>');
+  });
+  $('#designer').click(function() {
+    $('#content').append('<p>We dont have designer</p>');
+  });
+  $('#popular').click(function() {
+    $('#content').append('<p>We are not popular</p>');
+  });
 
 });
