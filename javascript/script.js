e23b7884f24552131c228a7e28e3d290276a24af
FedorTrain <fedya_dryagin@mail.ru>
2019-10-24 04:17:25 +0500

    g

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,15 @@
 $('document').ready(function() {
   $('#new').click(function() {
-    $('#content').append('<p>We dont have news</p>');
+    $('#content').append('<p>We dont have news!</p>');
   });
   $('#stock').click(function() {
-    $('#content').append('<p>We dont have stock</p>');
+    $('#content').append('<p>We dont have stock!</p>');
   });
   $('#designer').click(function() {
-    $('#content').append('<p>We dont have designer</p>');
+    $('#content').append('<p>We dont have designer!</p>');
   });
   $('#popular').click(function() {
-    $('#content').append('<p>We are not popular</p>');
+    $('#content').append('<p>We are not popular!</p>');
   });
 
 });
