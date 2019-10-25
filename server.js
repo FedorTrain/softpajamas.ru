692042880b0debe9af0e416177e3992e80c66ad9
FedorTrain <fedya_dryagin@mail.ru>
2019-10-26 04:50:52 +0500

    Fil

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send('work?'));
+mongoose.connect('mongodb://localhost:27017/online-store');
+const Product = mongoose.model('Product', {
+  id: Number,
+  name: String,
+  prise: mongoose.Schema.Types.Decimal128,
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('work'));
+
+app.get(
+  '/products',
+  (req, res) => Product.find()
+    .exec()
+    .then(products => res.json(products)),
+);
+
+app.post(
+  '/products',
+  (req, res) => Product.create(req.body)
+    .then(createProduct => res.json(createProduct)),
+);
+
+app.put(
+  '/products/:id',
+  (req, res) => Product.findOneAndUpdate({ id: req.params.id }, req.body)
+    .exec()
+    .then(product => res.json(product)),
+ );
+
+app.delete(
+  '/products/:id',
+  (req, res) => Product.deleteOne({id: req.params.id})
+    .exec()
+    .then(() => res.json({ success: true })),
+);
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
