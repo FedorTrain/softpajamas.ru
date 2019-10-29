217bb614a76b7ce8f06b8a651c2ee7a0abdb589b
FedorTrain <fedya_dryagin@mail.ru>
2019-10-29 06:31:42 +0500

    Good, only don't stop

--- check.php
+++ check.php
@@ -33,12 +33,12 @@
         $price= filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
         $image = filter_var(trim($_POST['image']), FILTER_SANITIZE_STRING);
 
-        // $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-        // $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
-        // VALUES('$name','$price','$image')");
-        // $musql->close();
+        $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
+        $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
+        VALUES('$name','$price','$image')");
+        $musql->close();
         echo $name;
-        //header('Location: /');
+        header('Location: /');
       ?>
 
     </div>
