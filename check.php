7524645f2abebed1b9189138346eb88ae211addd
FedorTrain <fedya_dryagin@mail.ru>
2019-10-29 06:21:42 +0500

    fucken 4 try

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
 <?php
-  $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
-  $price= filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
-  $image = filter_var(trim($_POST['image']), FILTER_SANITIZE_STRING);
+  $name = filter_var(trim($_GET['name']), FILTER_SANITIZE_STRING);
+  $price= filter_var(trim($_GET['price']), FILTER_SANITIZE_STRING);
+  $image = filter_var(trim($_GET['image']), FILTER_SANITIZE_STRING);
 
   // $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
   // $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
