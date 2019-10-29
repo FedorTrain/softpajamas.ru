d8c2183f936e4b24439d11a203742a09aa82426b
FedorTrain <fedya_dryagin@mail.ru>
2019-10-29 06:53:43 +0500

    F

--- check.php
+++ check.php
@@ -32,17 +32,12 @@
         $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
         $price= filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
         $image = filter_var(trim($_POST['image']), FILTER_SANITIZE_STRING);
-        echo $name;
 
-        $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-        echo $name;
-
+        $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
         $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
         VALUES('$name','$price','$image')");
-        echo $name;
 
         $mysql->close();
-        echo $name;
         header('Location: /');
       ?>
 
