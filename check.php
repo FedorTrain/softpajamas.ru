518e581aa77e62ae60f28174faa087025bdb2a5d
FedorTrain <fedya_dryagin@mail.ru>
2019-10-29 06:15:29 +0500

    try 2

--- check.php
+++ check.php
@@ -3,10 +3,10 @@
   $price= filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
   $image = filter_var(trim($_POST['image']), FILTER_SANITIZE_STRING);
 
-  $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-  $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
-  VALUES('$name','$price','$image')");
-  $musql->close();
-
-  header('Location: /');
+  // $mysql = new mysqli('server252.hosting.reg.ru', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
+  // $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
+  // VALUES('$name','$price','$image')");
+  // $musql->close();
+  echo $name;
+  //header('Location: /');
 ?>
