e00b8fa0ee41315a60351db9c875ca52c393df6f
FedorTrain <fedya_dryagin@mail.ru>
2019-10-30 03:03:21 +0500

    for image

--- php/check.php
+++ php/check.php
@@ -31,7 +31,8 @@
       <?php
         $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
         $price= filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
-        $image = filter_var(trim($_POST['image']), FILTER_SANITIZE_STRING);
+        $image = $_FILES['image']['name'];
+        $image = addslashes(file_get_contents($image));
 
         $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
         $mysql->query("INSERT INTO `products` (`name`, `price`, `image`)
