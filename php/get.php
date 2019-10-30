a24cf1b82d5ba1309c290c4bceb62e38723add09
FedorTrain <fedya_dryagin@mail.ru>
2019-10-30 07:37:07 +0500

    se

--- php/get.php
+++ php/get.php
@@ -2,7 +2,7 @@
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
   echo "Go";
 
-  $result = $mysql->query("SELECT * FROM `product` WHERE `id` = `2`");
+  $result = $mysql->query("SELECT * FROM `product` WHERE `id` = 2");
   if(!$result) {
     echo "OPA";
   } else {
