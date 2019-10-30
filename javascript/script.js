a912c394ac20485ef1a26fdb89f0ff8238fa960e
FedorTrain <fedya_dryagin@mail.ru>
2019-10-30 15:42:11 +0500

    g

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,28 @@
 $('document').ready(function() {
-  // var product;
-  // $.ajax({
-  //   type: "POST",
-  //   url: './php/get.php',
-  //   dataType: 'json',
-  //   data: {id: 2},
-  //   success: function (data) {
-  //               product = data;
-  //             }
-  // });
+  var product;
+  $.ajax({
+    type: "POST",
+    url: './php/get.php',
+    dataType: 'json',
+    data: {id: 2},
+    success: function (data) {
+                product = data;
+              }
+  });
 
 
-  $('#content').append('<div class="product"><img src="' + './images/rrr.png"' + ' alt="Error" /><p align="center">Name</p><p align="center">400p</p></div>');
-  $('#content').append('<div class="product"><img src="./images/' + product[image] + ' alt="Error"/><p align="center">' + product[name] + '</p><p align="center">' + product[price] '</p></div>');
+  $('#content').append(
+    '<div class="product">'+
+      '<img src="' + './images/rrr.png"' + ' alt="Error"/>'+
+      '<p align="center">Name</p>'+
+      '<p align="center">400p</p>'+
+    '</div>');
+  $('#content').append(
+    '<div class="product">'+
+      '<img src="./images/' + product[image] + ' alt="Error"/>'+
+      '<p align="center">' + product[name] + '</p>'+
+      '<p align="center">' + product[price] + '</p>'+
+    '</div>');
 
 
 
