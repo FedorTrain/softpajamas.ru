50ec5a14eb613149e1b2e38a8524235f830173e5
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 02:51:06 +0500

    1003

--- php/get.php
+++ php/get.php
@@ -5,7 +5,7 @@
 
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
 
-  $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
+  $result = $mysql->query("SELECT * FROM `product` WHERE `id` = 2");
   $mysql->close();
 
   // if(!$result) {
