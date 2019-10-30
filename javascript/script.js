4c4f2e8f859852ed3ade7152a28ac7402ab39a69
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 03:25:16 +0500

    1013

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,48 @@
 $('document').ready(function() {
-  var id;
-  var name;
-  var price;
-  var image;
-
+  let dproduct = new Object();
+  var name = 'pajamas';
+  var price = 1000;
+  var image = 'ppp.png';
+  alert('go');
   $.ajax({
     type: "POST",
     url: './php/get.php',
     dataType: 'json',
     data: {id: 2},
     success: function (data, textStatus, jqXHR) {
-      alert('good');
-      id = data.id;
       name = data.name;
       price = data.price;
       image = data.image;
     },
     error: function (jqXHR, textStatus, errorThrown){
       alert('error');
+      alert(textStatus);
+      alert(errorThrown);
     }
   });
+  // $.post(
+  //   "/var/www/u0842107/data/www/softpajamas.ru/php/get.php",
+  //   {id: 2},
+  //   getData
+  // );
+  //
+  // function getData(data)
+  // {
+  //   alert(data);
+  //   dproduct = data;
+  // };
+  alert('end');
 
 
   $('#content').append(
     '<div class="product">'+
-      '<img src="./images/' + 'rrr.png"' + ' alt="Error"/>'+
+      '<img src="' + './images/rrr.png"' + ' alt="Error"/>'+
       '<p align="center">Name</p>'+
       '<p align="center">400p</p>'+
     '</div>');
   $('#content').append(
     '<div class="product">'+
-      '<img src="./images/' + image + '" alt="Error"/>'+
+      '<img src="./images/' + image + ' alt="Error"/>'+
       '<p align="center">' + name + '</p>'+
       '<p align="center">' + price + '</p>'+
     '</div>');
