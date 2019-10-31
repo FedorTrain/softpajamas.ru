54ad8b143e885934799fe0ff31f7fc14d3e5860a
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 10:01:46 +0500

    count3

--- php/count.php
+++ php/count.php
@@ -4,7 +4,7 @@
 
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
 
-  $result = $mysql->query("SELECT COUNT(1) FROM `product`");
+  $result = $mysql->query("SELECT COUNT(*) FROM `product`");
 
   $mysql->close();
 
