962b6b46b05b3b5b30b115bd6882c7a671766397
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 10:12:57 +0500

    count test

--- php/count.php
+++ php/count.php
@@ -1,13 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">
+    <title>SoftPajamas</title>
+    <link href="styles/main.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="javascript/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="javascript/main.js"></script>
+  </head>
+  <body>
+    <div id="header">
+      <p align="right">
+        +7 9xx xxx xx xx<br>
+        sanya@mail.ru
+      </p>
 
-  header('Content-Type: application/json');
+      <br><br><br>
+      <a href="http://softpajamas.ru">SoftPajamas</a>
+      <br><br><br><br>
 
-  $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
+    </div>
+    <div id="sidebar">
+      <a href="stock.html"class="sidebar_botton">В наличии</a>
+      <a href="new.html"class="sidebar_botton">Новинки</a>
+      <a href="popular.html"class="sidebar_botton">Популярно</a>
+      <a href="designer.html"class="sidebar_botton">Заказать</a>
 
-  $result = $mysql->query("SELECT COUNT(*) FROM `product`");
+    </div>
+    <div id="content">
+      <?php
 
-  $mysql->close();
+        header('Content-Type: application/json');
 
-  echo $result;
+        $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
 
-?>
+        $result = $mysql->query("SELECT COUNT(*) FROM `product`");
+
+        $mysql->close();
+
+        echo $result;
+        
+      ?>
+
+    </div>
+
+ </body>
+</html>
