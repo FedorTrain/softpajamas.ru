f0c6127cbd360b335a04261ee570fc2fc97ba56e
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 07:02:44 +0500

    without php

--- product.php
+++ product.php
@@ -27,23 +27,7 @@
 
     </div>
     <div id="content">
-      <?php
-        $id = $_GET['id'];
-
-        $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-        $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
-        $mysql->close();
-        $product = $result->fetch_assoc();
-
-        echo '<div class="product">' .
-            '<img src="./images/' . $product['image'] . '" alt="Error"/>' .
-            '<p align="center">' . $product['image'] . '</p>' .
-            '<p align="center">' . $product['image'] . 'р</p>' .
-            '</div>');
-
-
-
-       ?>
+      
     </div>
 
  </body>
