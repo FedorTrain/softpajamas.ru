522d2c3fd24c6fd3f5216da8160d2f0aa84a6367
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 10:25:45 +0500

    h

--- php/count.php
+++ php/count.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html >
-  <head>
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-    <title>SoftPajamas</title>
-    <link href="styles/main.css" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="javascript/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="javascript/main.js"></script>
-  </head>
-  <body>
-    <div id="header">
-      <p align="right">
-        +7 9xx xxx xx xx<br>
-        sanya@mail.ru
-      </p>
 
-      <br><br><br>
-      <a href="http://softpajamas.ru">SoftPajamas</a>
-      <br><br><br><br>
-
-    </div>
-    <div id="sidebar">
-      <a href="stock.html"class="sidebar_botton">В наличии</a>
-      <a href="new.html"class="sidebar_botton">Новинки</a>
-      <a href="popular.html"class="sidebar_botton">Популярно</a>
-      <a href="designer.html"class="sidebar_botton">Заказать</a>
-
-    </div>
-    <div id="content">
       <?php
 
         header('Content-Type: application/json');
@@ -39,11 +10,6 @@
         $mysql->close();
         $product = $result->fetch_assoc();
 
-        echo $product;
+        echo json_encode($product);
 
       ?>
-
-    </div>
-
- </body>
-</html>
