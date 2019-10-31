5d6b8ad17d4720e338d6d6dbefd531b720956e5b
FedorTrain <fedya_dryagin@mail.ru>
2019-10-31 09:27:14 +0500

    go 100

--- javascript/main.js
+++ javascript/main.js
@@ -43,7 +43,7 @@ function product(id) {
 $('document').ready(function() {
 
   var i;
-  var end = 1000;
+  var end = 100;
   for (i = 0; i < end; i++) {
     if (product(i)) end++;
     //inputProduct('rrr.png', 'Name', 1234);
