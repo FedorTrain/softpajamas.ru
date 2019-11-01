53ccfcbb912b51930bfadb873cf2780ca3967a51
FedorTrain <fedya_dryagin@mail.ru>
2019-11-01 22:19:26 +0500

    13

--- php/load_id.php
+++ php/load_id.php
@@ -2,15 +2,15 @@
   header('Content-Type: application/json');
 
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-  $result = $mysql->query("SELECT id FROM product");
+  $result = $mysql->query("SELECT `id` FROM `product`");
   $mysql->close();
 
-  $products = $result->fetch_assoc();
-  $i = 0;
-  foreach ($products as $product) {
-    $_id_[$i] = $i;
-    $i++;
-  }
+  $_id_ = $result->fetch_assoc();
+  // $i = 0;
+  // foreach ($products as $product) {
+  //   $_id_[$i] = $product;
+  //   $i++;
+  // }
 
   echo json_encode($_id_);
 
