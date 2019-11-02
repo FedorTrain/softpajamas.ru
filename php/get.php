09ca793daef1cb97b62654d6450676391f33cd1e
FedorTrain <fedya_dryagin@mail.ru>
2019-11-02 20:35:43 +0500

    debag get

--- php/get.php
+++ php/get.php
@@ -2,12 +2,14 @@
   header('Content-Type: application/json');
 
   $id = $_POST['id'];
-
+  echo 'id = ' . $id;
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
 
   $result = $mysql->query("SELECT * FROM `products` WHERE `id` = $id");
   $mysql->close();
   $product = $result->fetch_assoc();
+  echo 'good connect';
+  echo $product;
   echo json_encode($product);
 
 ?>
