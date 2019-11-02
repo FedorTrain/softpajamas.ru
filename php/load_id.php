3dd1a5fe7c3171c0e4978cb111cb623de7de60cc
FedorTrain <fedya_dryagin@mail.ru>
2019-11-02 20:24:42 +0500

    new infa product load_id

--- php/load_id.php
+++ php/load_id.php
@@ -2,7 +2,7 @@
   header('Content-Type: application/json');
 
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-  $result = $mysql->query("SELECT `id` FROM `product`");
+  $result = $mysql->query("SELECT `id` FROM `products`");
   $mysql->close();
 
   $_id_;
