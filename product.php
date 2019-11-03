467924e5b7683948b8fafad3813aa0e3b401dfe6
FedorTrain <fedya_dryagin@mail.ru>
2019-11-03 09:36:26 +0500

    try product.php

--- product.php
+++ product.php
@@ -31,7 +31,7 @@
         $id = $_GET['id'];
 
         $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
-        $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
+        $result = $mysql->query("SELECT * FROM `products` WHERE `id` = $id");
         $mysql->close();
         $product = $result->fetch_assoc();
 
@@ -44,8 +44,6 @@
          <p>' . $product['name'] . '</p>
          <p>' . $product['price'] . 'р</p>
          </div>';
-
-
 
        ?>
     </div>
