301334fab8f553179a48811277c985563dd5f52f
FedorTrain <fedya_dryagin@mail.ru>
2019-11-03 09:38:36 +0500

    try again

--- php/get.php
+++ php/get.php
@@ -5,7 +5,7 @@
   //echo 'id = ' . $id;
   $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
 
-  $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
+  $result = $mysql->query("SELECT * FROM `products` WHERE `id` = $id");
   $mysql->close();
   $product = $result->fetch_assoc();
   //echo 'good connect';
