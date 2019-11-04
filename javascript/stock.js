60d1a9a1274ff2c4624ea89c67333dfcbfe46d2f
FedorTrain <fedya_dryagin@mail.ru>
2019-11-04 16:37:23 +0500

    2.1

--- javascript/stock.js
+++ javascript/stock.js
@@ -29,7 +29,7 @@ function product(id) {
       info = data.info;
       type = data.type;
       if (data.number > 0) {
-        if (number > 0) inputProduct(id, image, name, price, info, type, number);
+        inputProduct(id, image, name, price, info, type, number);
       }
       get = true;
     },
