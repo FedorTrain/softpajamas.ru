239648a12df0685e4c8f66a2e40384a8aaa96373
FedorTrain <fedya_dryagin@mail.ru>
2019-11-04 16:55:47 +0500

    delete

--- php/delete.php
+++ php/delete.php
@@ -15,7 +15,7 @@
           $id = $_POST['id'];
           $mysql = new mysqli('localhost', 'u0842107_admin', '2Q0n1R1h', 'u0842107_products');
           $result = $mysql->query("SELECT * FROM `product` WHERE `id` = $id");
-          $mysql->query("DELETE FROM product WHERE id = $id");
+          $mysql->query("DELETE FROM products WHERE id = $id");
           $mysql->close();
           $product = $result->fetch_assoc();
 
