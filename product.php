6836d0a083fa10145531e69aef77ba9e3970bc6c
FedorTrain <fedya_dryagin@mail.ru>
2019-11-27 16:43:29 +0500

    5

--- product.php
+++ product.php
@@ -42,10 +42,10 @@
         echo '<div class="product">
         <div class="container">
           <div class="row">
-           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+           <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
              <img src="/images/' . $product['image'] . '" alt="Error"/>
            </div>
-           <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+           <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
              <form action="" method="post" enctype="multipart/form-data">
                <button class="btn btn-success">Купить</button>
              </form>
