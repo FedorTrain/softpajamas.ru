c28e7b49c26cf62d7bc3c8bb74976a3537f66821
FedorTrain <fedya_dryagin@mail.ru>
2019-11-28 14:46:38 +0500

    mail php

--- pay.php
+++ pay.php
@@ -13,7 +13,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/javascript/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="/javascript/main.js"></script>
   </head>
   <body>
     <div id="header">
@@ -37,25 +36,11 @@
     <div id="content">
       <div class="container">
         <div class="row">
-          <?php
-            ini_set( 'display_errors', 1 );
-
-            error_reporting( E_ALL );
-
-            $from = "testing@yourdomain";
-
-            $to = "recipientemailaddress";
-
-            $subject = "Checking PHP mail";
-
-            $message = "PHP mail works just fine";
-
-            $headers = "From:" . $from;
-
-            mail($to,$subject,$message, $headers);
-
-            echo "The email message was sent.";
-          ?>
+          <form action="php/mail.php" method="post">
+            <input type="text" name="name" placeholder="Имя">
+            <textarea name="msg" placeholder="Сообщение"></textarea>
+            <input type="submit" name="otprav" value="Отправить">
+          </form>
         </div>
       </div>
     </div>
