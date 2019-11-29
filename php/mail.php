1c33a7760d0fa5255c20c71d85c87b4de2335945
FedorTrain <fedya_dryagin@mail.ru>
2019-11-30 01:40:02 +0500

    try 2

--- php/mail.php
+++ php/mail.php
@@ -7,16 +7,19 @@
     $headerss ="Content-type: text/html; charset=$charset\r\n";
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
+    
     $name = htmlspecialchars($_POST["name"]);
     $phone = htmlspecialchars($_POST["phone"]);
     $mail = htmlspecialchars($_POST["mail"]);
     $vk = htmlspecialchars($_POST["vk"]);
     $comm = htmlspecialchars($_POST["msg"]);
+
     $name = urldecode($_POST["name"]);
     $phone = urldecode($_POST["phone"]);
     $mail = urldecode($_POST["mail"]);
     $vk = urldecode($_POST["vk"]);
     $comm = urldecode($_POST["msg"]);
+
     $msg = "Имя: " . $name . "\n";
     $msg .= "Телефон: " . $phone . "\n";
     $msg .= "Электронная почта: " . $mail . "\n";
