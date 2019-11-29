4d54cae5a8d1fb7dc8fad5ed4420e24c87cd9f6d
FedorTrain <fedya_dryagin@mail.ru>
2019-11-30 01:36:40 +0500

    final

--- php/mail.php
+++ php/mail.php
@@ -7,11 +7,21 @@
     $headerss ="Content-type: text/html; charset=$charset\r\n";
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
-    $msg = "Имя: " . $_POST["name"] . "\n";
-    $msg .= "Телефон: " . $_POST["phone"] . "\n";
-    $msg .= "Электронная почта: " . $_POST["mail"] . "\n";
-    $msg .= "Вконтакте: " . $_POST["vk"] . "\n";
-    $msg .= "Комментарий: " . $_POST["msg"] . "\n";
+    $name = htmlspecialchars($_POST["name"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $mail = htmlspecialchars($_POST["mail"]);
+    $vk = htmlspecialchars($_POST["vk"]);
+    $comm = htmlspecialchars($_POST["msg"]);
+    $name = urldecode($_POST["name"]);
+    $phone = urldecode($_POST["phone"]);
+    $mail = urldecode($_POST["mail"]);
+    $vk = urldecode($_POST["vk"]);
+    $comm = urldecode($_POST["msg"]);
+    $msg = "Имя: " . $name . "\n";
+    $msg .= "Телефон: " . $phone . "\n";
+    $msg .= "Электронная почта: " . $mail . "\n";
+    $msg .= "Вконтакте: " . $vk . "\n";
+    $msg .= "Комментарий: " . $comm . "\n";
     mail($to, $subject, $msg, $headerss);
     header('Location:/sections/thanks.html');
 
