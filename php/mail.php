9fd1714977540372941b5383fb71bc85137e2540
FedorTrain <fedya_dryagin@mail.ru>
2019-11-30 01:54:20 +0500

    5

--- php/mail.php
+++ php/mail.php
@@ -8,25 +8,28 @@
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
 
+    $name = urldecode($_POST["name"]);
+    $phone = urldecode($_POST["phone"]);
+    $mail = urldecode($_POST["mail"]);
+    $vk = urldecode($_POST["vk"]);
+    $comm = urldecode($_POST["msg"]);
+    
     $name = htmlspecialchars($_POST["name"]);
     $phone = htmlspecialchars($_POST["phone"]);
     $mail = htmlspecialchars($_POST["mail"]);
     $vk = htmlspecialchars($_POST["vk"]);
     $comm = htmlspecialchars($_POST["msg"]);
 
-    $name = urldecode($_POST["name"]);
-    $phone = urldecode($_POST["phone"]);
-    $mail = urldecode($_POST["mail"]);
-    $vk = urldecode($_POST["vk"]);
-    $comm = urldecode($_POST["msg"]);
+
 
     $msg = "Имя: " . $name . "\n";
     $msg .= "Телефон: " . $phone . "\n";
     $msg .= "Электронная почта: " . $mail . "\n";
     $msg .= "Вконтакте: " . $vk . "\n";
     $msg .= "Комментарий: " . $comm . "\n";
-    echo mail($to, $subject, $msg, $headerss);
-    // header('Location:/sections/thanks.html');
+
+    mail($to, $subject, $msg, $headerss);
+    header('Location:/sections/thanks.html');
 
   }
 ?>
