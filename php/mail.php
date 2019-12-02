5fa9abd7d885f5dc3584118c30d9a645e942e821
FedorTrain <fedya_dryagin@mail.ru>
2019-12-02 21:59:37 +0500

    31

--- php/mail.php
+++ php/mail.php
@@ -1,18 +1,18 @@
 <?php
   if (isset($_POST["otprav"])) {
     $id = $_GET['id'];
-    $to = "fedor.dryagin@gmail.com";
+    $to = "fedya_dryagin@mail.ru";
     $subject = "Письмо с сайта";
     $charset = "utf-8";
     $headerss ="Content-type: text/html; charset=$charset\r\n";
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
 
-    $name = htmlspecialchars($_POST["name"]);
-    $phone = htmlspecialchars($_POST["phone"]);
-    $mail = htmlspecialchars($_POST["mail"]);
-    $vk = htmlspecialchars($_POST["vk"]);
-    $comm = htmlspecialchars($_POST["msg"]);
+    $name = htmlspecialchars(trim($_POST["name"]));
+    $phone = htmlspecialchars(trim($_POST["phone"]));
+    $mail = htmlspecialchars(trim($_POST["mail"]));
+    $vk = htmlspecialchars(trim($_POST["vk"]));
+    $comm = htmlspecialchars(trim($_POST["msg"]));
 
     $name = substr(urldecode($name), 0, 20);
     $phone = substr(urldecode($phone), 0, 15);
