c37257d7869e8d5ee5677f79d90ec7e50df173f7
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 03:39:21 +0500

    17

--- f/ss.js
+++ f/ss.js
@@ -1,13 +1,15 @@
 function submit(){
-    var oc = [];
-    var data = $('input[name*="ocs"]').val();
-    for (var i = 0; i < oc.length; i++) {
-      if (data[i] == '2') oc.push(2);
-      if (data[i] == '3') oc.push(3);
-      if (data[i] == '4') oc.push(4);
-      if (data[i] == '5') oc.push(5);
-    }
+  $('#content').html('oc[0]');
 
-    $('#content').append(oc[0]);
+  var oc = [];
+  var data = $('input[name*="ocs"]').val();
+  for (var i = 0; i < oc.length; i++) {
+    if (data[i] == '2') oc.push(2);
+    if (data[i] == '3') oc.push(3);
+    if (data[i] == '4') oc.push(4);
+    if (data[i] == '5') oc.push(5);
+  }
+
+  $('#content').html(oc[0]);
 
 }
