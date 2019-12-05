41a595bfd18fff06b6bf254163e0e11fa48fc436
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 04:11:05 +0500

    27

--- f/ss.js
+++ f/ss.js
@@ -1,10 +1,8 @@
-function submit(){
-  $('#res').html('oc[0]');
+$("#ocs").submit(function() {
 
   var oc = [];
 
-  var data = $('input[name*="ocs"]').val();
-  $('#res').html(data);
+  var data = $('input[name*="ocsi"]').val();
 
   var i;
   var res = 0;
@@ -22,4 +20,28 @@ function submit(){
 
   $('#res').html(res);
 
-}
+});
+
+// function submit(){
+//
+//   var oc = [];
+//
+//   var data = $('input[name*="ocs"]').val();
+//
+//   var i;
+//   var res = 0;
+//   for (i = 0; i < data.length; i++) {
+//     if (data[i] == '2') oc.push(2);
+//     if (data[i] == '3') oc.push(3);
+//     if (data[i] == '4') oc.push(4);
+//     if (data[i] == '5') oc.push(5);
+//   }
+//   for (i = 0; i < oc.length; i++) {
+//     res += oc[i];
+//   }
+//
+//   res /= i;
+//
+//   $('#res').html(res);
+//
+// }
