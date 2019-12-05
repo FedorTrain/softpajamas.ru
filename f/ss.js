88219c6367be57f035f4c029824ddcf49817e131
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 03:07:49 +0500

    4

--- f/ss.js
+++ f/ss.js
@@ -1,10 +1,10 @@
 $('document').ready(function() {
   $('#ocs').onsubmit(function(e) {
     var form_data = $("#ocs").serializeArray;
-    alert("fff");
     alert(form_data);
-    $('#content').append(
 
+    $('#content').append(
+      'hhhh';
 
     );
   );
