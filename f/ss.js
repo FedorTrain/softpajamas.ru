9c5153f0cf8afb10d517fea758d8bac18113315c
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 03:38:13 +0500

    16

--- f/ss.js
+++ f/ss.js
@@ -1,7 +1,7 @@
 function submit(){
     var oc = [];
     var data = $('input[name*="ocs"]').val();
-    for (var i = 0; i < ocs.length; i++) {
+    for (var i = 0; i < oc.length; i++) {
       if (data[i] == '2') oc.push(2);
       if (data[i] == '3') oc.push(3);
       if (data[i] == '4') oc.push(4);
