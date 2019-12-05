811d3ca0ca9c31a9dbe5a8ee24cf8cbe54e049bc
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 03:43:46 +0500

    21

--- f/ss.js
+++ f/ss.js
@@ -5,6 +5,7 @@ function submit(){
 
   var data = $('input[name*="ocs"]').val();
   $('#res').html(data);
+  
 
   for (var i = 0; i < oc.length; i++) {
     if (data[i] == '2') $('#res').html('dddd');
