f12abba670aab5083d938b9c05aac4adf7a2cd11
FedorTrain <fedya_dryagin@mail.ru>
2019-12-06 03:26:30 +0500

    9

--- f/ss.js
+++ f/ss.js
@@ -1,12 +1,19 @@
-$(function(){
-  $("#ocs").onsubmit(function(){
-    var form_data = $("#ocs").serializeArray;
+// $(function(){
+//   $("#ocs").onsubmit(function(){
+//     var form_data = $("#ocs").serializeArray;
+//
+//     alert(form_data);
+//
+//
+//   });
+// });
 
-    alert(form_data);
+function submit(){
+    // var login = $('input[name*="login"]').val();
+    // var pwd = $('input[name*="password"]').val();
+    $('#content').append('hhhh');
 
-
-  });
-});
+}
 
 $('document').ready(function() {
 
