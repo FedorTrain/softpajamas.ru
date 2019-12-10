5387c9c0e43671e648d6caf72bda58c4ce2b1fec
FedorTrain <fedya_dryagin@mail.ru>
2019-12-10 08:17:50 +0500

    mail

--- php/mail.php
+++ php/mail.php
@@ -3,7 +3,7 @@
     $id = $_GET['id'];
     $to = "fedya_dryagin@mail.ru";
     $subject = "Письмо с сайта";
-    $charset = "utf-8";
+    $charset = "iso-8859-1";
     $headerss ="Content-type: text/html; charset=$charset\r\n";
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
