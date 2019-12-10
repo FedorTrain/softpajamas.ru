5964cf000e6668276f1b201e8e696b0f45362bcb
FedorTrain <fedya_dryagin@mail.ru>
2019-12-10 09:07:49 +0500

    4

--- php/mail.php
+++ php/mail.php
@@ -5,7 +5,7 @@
     $subject = "Письмо с сайта";
     $charset = "utf-8";
     $headerss ="Content-type: text/html; charset=$charset\r\n";
-    // $headerss.="MIME-Version: 1.0\r\n";
+    $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
 
     $name = htmlspecialchars(trim($_POST["name"]));
