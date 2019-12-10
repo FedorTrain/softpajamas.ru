9775ae43bb10654eefb0ee6262a4c5621b089cd8
FedorTrain <fedya_dryagin@mail.ru>
2019-12-10 08:42:17 +0500

    3

--- php/mail.php
+++ php/mail.php
@@ -5,7 +5,7 @@
     $subject = "Письмо с сайта";
     $charset = "utf-8";
     $headerss ="Content-type: text/html; charset=$charset\r\n";
-    $headerss.="MIME-Version: 1.0\r\n";
+    // $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
 
     $name = htmlspecialchars(trim($_POST["name"]));
@@ -28,7 +28,7 @@
     $msg .= "Комментарий: " . $comm . "<br />";
     $msg .= "id товара: " . $id . "<br />";
 
-    mail($to, $subject, "fedya_dryagin@mail.ru", $headerss);
+    mail($to, $subject, $msg, $headerss);
     header('Location:/sections/thanks.html');
 
   }
