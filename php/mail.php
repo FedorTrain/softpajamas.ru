9da245440cc6bdb2d646acc929da38313e10799d
FedorTrain <fedya_dryagin@mail.ru>
2019-12-10 08:32:29 +0500

    4

--- php/mail.php
+++ php/mail.php
@@ -10,20 +10,20 @@
     $headerss.="Content-type: text/html; charset=$charset\r\n";
     $headerss.="MIME-Version: 1.0\r\n";
     $headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
-    echo "1";
+
     $name = htmlspecialchars(trim($_POST["name"]));
     $phone = htmlspecialchars(trim($_POST["phone"]));
     $mail = htmlspecialchars(trim($_POST["mail"]));
     $vk = htmlspecialchars(trim($_POST["vk"]));
     $comm = htmlspecialchars(trim($_POST["msg"]));
-echo "2";
+
     $msg = "Имя: " . $name . "<br />";
     $msg .= "Телефон: " . $phone . "<br />";
     $msg .= "Электронная почта: " . $mail "<br />";
     $msg .= "Вконтакте: " . $vk . " <br />";
     $msg .= "Комментарий: " . $comm . "<br />";
     $msg .= "id товара: " . $id . "<br />";
-echo "3";
+
     mail($to, $subject, $msg, $headerss);
     header('Location:/sections/thanks.html');
 
