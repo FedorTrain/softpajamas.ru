9e0b5688c48bbeadb6f91b95faef5b83764cb884
FedorTrain <fedya_dryagin@mail.ru>
2020-02-11 08:17:37 +0500

    g

--- games/one.js
+++ games/one.js
@@ -27,8 +27,10 @@ document.addEventListener("keydown", moveUp);
 document.addEventListener("touchstart", moveUp);
 
 function moveUp() {
- yPos -= 25;
- fly.play();
+   if (yPos == cvs.height - fg.height) {
+     v = 100;
+     fly.play();
+   }
 }
 
 // Создание блоков
@@ -43,43 +45,47 @@ var score = 0;
 // Позиция птички
 var xPos = 10;
 var yPos = 150;
-var grav = 1.5;
+var g = 40;
+var t = 0.1;
+var v = 0;
+var game = true;
 
 function draw() {
  ctx.drawImage(bg, 0, 0);
 
  for(var i = 0; i < pipe.length; i++) {
- ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
- pipe[i].x--;
-
+ if (game) {
+   pipe[i].x--;
+ }
  if(pipe[i].x == 125) {
  pipe.push({
  x : cvs.width,
- y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+ y : 200
  });
  }
 
  // Отслеживание прикосновений
- if(xPos + bird.width >= pipe[i].x
- && xPos <= pipe[i].x + pipeUp.width
- && (yPos <= pipe[i].y + pipeUp.height
- || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
- location.reload(); // Перезагрузка страницы
+ if(xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width) {
+  // game = false; // Перезагрузка страницы
  }
 
  if(pipe[i].x == 5) {
- score++;
- score_audio.play();
- }
+   score++;
+   score_audio.play();
+   }
  }
 
  ctx.drawImage(fg, 0, cvs.height - fg.height);
  ctx.drawImage(bird, xPos, yPos);
 
- yPos += grav;
-
+ v = v - g * t;
+ s = v * t - g * t*t / 2;
+ yPos -= s;
+ if (yPos > cvs.height - fg.height){
+  yPos = cvs.height - fg.height;
+ }
  ctx.fillStyle = "#000";
  ctx.font = "24px Verdana";
  ctx.fillText("Счет: " + score, 10, cvs.height - 20);
